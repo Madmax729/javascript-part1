6b5475adf6fe14d070cb8f66e9317528224ad084
Madmax729 <135532080+Madmax729@users.noreply.github.com>
2024-01-15 17:26:04 +0000

    loops

--- 01_basics/text.js
+++ 01_basics/text.js
@@ -1,1 +1,24 @@
-console.log("hello there")
+// for of loops
+
+let str = "Sony"
+let size = 0
+for (let val of str){
+    // iterator -> charachter
+    console.log("val = " , val);
+    size++
+} 
+console.log("size = " , size)
+
+
+//  for in loop 
+
+let student = {                     //student object
+    name : "Mangesh",
+    age : 18,
+    cgpa : 8.5,
+    isPass: true
+}
+
+for (let key in student){
+    console.log(" key= ", key ,"value = ", student[key])
+}
