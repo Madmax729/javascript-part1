a5bcf61bbdd8d4fcf83064f1187f5a18829224d8
Madmax729 <135532080+Madmax729@users.noreply.github.com>
2024-01-16 17:06:20 +0000

    changes

---
+++ 02_basics/functions.js
@@ -0,0 +1,16 @@
+// functions
+
+// function sumNum(a,b){
+//     console.log(a+b)
+// }
+
+// sumNum(4,5)
+
+// Arrow function 
+const arrowSum = (a,b)  => {
+    console.log(a+b);
+};
+
+const arrowMulti = (a,b)=> {
+    console.log(a*b);
+};
